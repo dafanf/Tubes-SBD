53b4d91ab2582687d17bc18667e176ae036ef505
dafanf <dafanurulfauziansyah97@gmail.com>
2022-11-15 08:44:34 +0700

    adding connection to database and some config

---
+++ App/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL','http://localhost/web-laundry/Tubes-SBD');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'admin_laundry');
+define('DB_PASS', 'awaslupa');
+define('DB_NAME', 'xe');
+
+?>
